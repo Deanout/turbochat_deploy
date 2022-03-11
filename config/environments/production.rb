5ccb5cdb964b3eacd72558ce5cce41ee55e71916
Dean DeHart <DeHartDean@gmail.com>
2022-03-10 23:25:42 -0500

    Attempt default URL fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,21 @@ Rails.application.configure do
   config.active_storage.service = :digitalocean
   config.active_storage.variant_processor = :mini_magick
 
+  # If a default host is specifically defined then it's used otherwise the app is
+  # assumed to be a Heroku review app. Note that `Hash#fetch` is used defensively
+  # so the app will blow up at boot-time if both `DEFAULT_URL_HOST` and
+  # `HEROKU_APP_NAME` aren't defined.
+  host = ENV['DEFAULT_URL_HOST'] ||
+         "#{ENV.fetch('HEROKU_APP_NAME')}.herokuapp.com"
+
+  # Set the correct protocol as SSL isn't configured in development or test.
+  protocol = Rails.application.config.force_ssl ? 'https' : 'http'
+
+  config.action_controller.default_url_options = {
+    host: host,
+    protocol: protocol
+  }
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
